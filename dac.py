10841a43783a1c3fafa7230c07466623dc510822
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-07-14 10:19:36 -0700

    rm unused imports

--- dac.py
+++ dac.py
@@ -1,15 +1,15 @@
-from diagrams import Cluster, Diagram, Edge
+from diagrams import Diagram
 from diagrams.aws.network import APIGateway
 from diagrams.aws.compute import LambdaFunction
-from diagrams.aws.management import SystemsManagerParameterStore
 from diagrams.aws.devtools import Codebuild
 
 common_attr = {"fontsize": "15", "fontname": "Times bold"}
 
-
 with Diagram("terraform-aws-dynamic-github-source", graph_attr=common_attr):
     validator = LambdaFunction("Validate Requests", **common_attr)
-    runner = LambdaFunction("Starts CodeBuild project\nwith override config", labelloc="t", **common_attr)
+    runner = LambdaFunction(
+        "Starts CodeBuild project\nwith override config", labelloc="t", **common_attr
+    )
     codebuild = Codebuild("Build", **common_attr)
 
     APIGateway("Webhook Event", **common_attr) >> validator >> runner >> codebuild
